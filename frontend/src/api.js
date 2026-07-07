2d61fa095814a8fecf4766a944c04974ecfeebea
MarySophiaR <marysophia2905@gmail.com>
2026-07-07 09:29:39 +0530

    Fix production API base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://focusboard-backend-tiyg.onrender.com"
+    baseURL: "https://focusboard-backend-tiyg.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
